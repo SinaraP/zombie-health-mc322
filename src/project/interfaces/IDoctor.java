5b958bf9e7d3980c9e68ddf225750e94a7bb1a3d
Sinara C. Pamplona <50603935+SinaraP@users.noreply.github.com>
2019-05-14 10:46:00 -0300

    Update IDoctor.java

--- src/project/interfaces/IDoctor.java
+++ src/project/interfaces/IDoctor.java
@@ -1,5 +1,7 @@
 package project.interfaces;
 
 public interface IDoctor extends IEnquirer, IAnswerReceptacle, ITableProducerReceptacle, ITreeProducerReceptacle{
-    
+    public void connect(IPatient patient);
+    public void connect(ITreeProducer tree);
+    public void connect(ITableProducer table);
 }
